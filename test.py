f4e62be9dc10dde06ecf685c9c7d4c64080cd88a
Tamer Abbass <tamerh.abbass@gmail.com>
2021-07-26 11:31:23 -0700

    Created sms_lead app and verified functionality of all core components

---
+++ test.py
@@ -0,0 +1,66 @@
+import csv
+import json
+import os
+import re
+import pandas
+import PureCloudPlatformClientV2
+import requests
+
+
+zillow_list_id = os.environ('Tricon_Zillow_Contact_ID')
+
+zillow_contact_list_url = os.environ('Tricon_Contact_List_URI')
+
+# Set Purecloud region
+region = PureCloudPlatformClientV2.PureCloudRegionHosts.us_west_2
+
+# Get API Host for Purecloud region
+PureCloudPlatformClientV2.configuration.host = region.get_api_host()
+
+# Create API Client and get client credentials with client ID and key
+apiclient = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(os.environ('Tricon_SMS_Client_ID'), os.environ('Tricon_SMS_Client_Secret'))
+print(apiclient.access_token)
+# # Create Outbound Api Client
+api_instance = PureCloudPlatformClientV2.ConversationsApi(apiclient)
+body = PureCloudPlatformClientV2.SendAgentlessOutboundMessageRequest()
+
+data = {
+  "fromAddress": os.environ('Tricon_fromAddress'),
+  "toAddress": os.environ('Tricon_toAddress'),
+  "toAddressMessengerType": "sms",
+  "textBody": "Hi, this is Tamer",
+}
+
+api_response = api_instance.post_conversations_messages_agentless(data)
+
+# # Grab Zillow_Master download URI
+# exporturi = api_instance.get_outbound_contactlist_export(zillow_list_id).to_dict()
+
+# # Grab CSV content by providing the download uri with authorization header
+# req = requests.get(exporturi['uri'], headers={'authorization': f'Bearer {apiclient.access_token}'}).text
+
+req = requests.post("https://api.usw2.pure.cloud/api/v2/conversations/messages/agentless", data, headers={'authorization': f'Bearer {apiclient.access_token}'})
+
+
+
+# # print()
+# rows = '{}'.format(req.replace('"','')).splitlines()
+# # print(rows)
+# contact_list = []
+# for x in rows:
+#     contact_list.append(x.split(','))
+
+
+
+# # zillow_data = [str(req.content).split('\n')]
+# # print(dir(zillow_data))
+
+
+
+# for x, contact in enumerate(contact_list[0]):
+#     print(contact)
+#     print(contact_list[1][x])
+#     print('\n')
+    
+# zillow_master = pandas.read_csv('Zillow_Master.csv')
+# print(zillow_master)
