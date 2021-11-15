b6c401874e648cc9fca287001050db0b223e2bbf
Tamer <tamerh.abbass@gmail.com>
2021-11-15 09:14:58 -0800

    Reporting object functioning

--- reporting/generate_report.py
+++ reporting/generate_report.py
@@ -7,148 +7,116 @@ import datetime
 import pandas
 import csv
 
-DAYS = 60
 CSV = ''
 
-
-def generate():
-    column_name = {'Number': '', 'First': '', 'Last': '', 'Address': '', 'City': '', 'State': '', 'Zip': '', 'PropertyID': '','SMSMessage1': '',  'updated': ''}
-    campaign_list = []
-    queryset = Campaign.objects.all()
+class GenerateReport:
     
-    count = 0
-    data_set = SMS_Successful.objects.all().filter(campaign=str('67544c3482904aa28cca1545ae9965d0')).values('Number', 'First', 'Last', 'Address', 'City', 'State', 'Zip', 'PropertyID','SMSMessage1',  'updated')
+    def __init__(self, campaign_name, columns, days):
+        self.columns = columns
+        self.campaign_name = campaign_name
+        self.campaign_id = None
+        self.queryset = None
+        self.count = 0
+        self.data_success = None
+        self.data_fail = None
+        self.file = None
+        self.numberDays = days
+
+
+    def getCampaignId(self):
+        qs = Campaign.objects.get(name=self.campaign_name)
+        self.campaign_id = qs.uuid.__str__().replace('-','')
+        return self.campaign_id
+
     
-    file = f"Rental_Forgiveness_Success_{str(datetime.datetime.now()).replace(':', '_').replace('.', '_')}.csv"
-    with open(file, 'w+', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=column_name)
-        writer.writeheader()
-        
-        writer.writerow(column_name)
-        for data in data_set.order_by('updated'):
-            if (datetime.datetime.now() - datetime.timedelta(days=DAYS)) < data['updated'].replace(tzinfo=None) and (datetime.datetime.now()) > data['updated'].replace(tzinfo=None):
-                count += 1
-                writer.writerow(data)
-    print("Rental Forgiveness Success:",count)
-    return file, count
+    def getSMSList(self):
+        self.data_fail = Sent_Call_List.objects.all().filter(campaign=self.campaign_id).values(*self.columns)
+        self.data_success = SMS_Successful.objects.all().filter(campaign=self.campaign_id).values(*self.columns)
+        return 1
+
+    def setReportFileName(self):
+        self.file = f"{self.campaign_name.replace(' ','_')}_{str(datetime.datetime.now()).replace(':', '_').replace('.', '_')}.csv"
+        return 0
+
+    def writeSMSByFilteredDate(self):
+        count_fail, count_success = 0, 0
+        with open(self.file, 'w+', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=self.columns)
+            writer.writeheader()
+            writer.writerow(self.columns)
+
+            # Process Failed
+            for data in self.data_fail.order_by('updated'):
+                if (datetime.datetime.now() - datetime.timedelta(days=self.numberDays)) < data['updated'].replace(tzinfo=None) and (datetime.datetime.now()) > data['updated'].replace(tzinfo=None):
+                        count_fail += 1
+                        writer.writerow(data)
+            print(f"{self.campaign_name} Failed:",count_fail)
+
+            # Process Success
+            for data in self.data_success.order_by('updated'):
+                if (datetime.datetime.now() - datetime.timedelta(days=self.numberDays)) < data['updated'].replace(tzinfo=None) and (datetime.datetime.now()) > data['updated'].replace(tzinfo=None):
+                        count_success += 1
+                        writer.writerow(data)
+            print(f"{self.campaign_name} Success:",count_success)
+        return self.file, count_fail, count_success
 
 
-def generate_2():
-    column_name = {'Number': '', 'First': '', 'Last': '', 'Address': '', 'City': '', 'State': '', 'Zip': '', 'PropertyID': '','SMSMessage1': '',  'updated': ''}
-    campaign_list = []
-    queryset = Campaign.objects.all()
-    count = 0
-    data_set = SMS_Successful.objects.all().filter(campaign=str('2bc8a390424f4ce68222e4bab536264b')).values('Number', 'First', 'Last', 'Address', 'City', 'State', 'Zip', 'PropertyID','SMSMessage1',  'updated')
-   
-    file = f"Zillow_Contacts_Success_{str(datetime.datetime.now()).replace(':', '_').replace('.', '_')}.csv"
-    with open(file, 'w+', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=column_name)
-        writer.writeheader()
-        
-        writer.writerow(column_name)
-        for data in data_set.order_by('updated'):
-            
-            if (datetime.datetime.now() - datetime.timedelta(days=DAYS)) < data['updated'].replace(tzinfo=None) and (datetime.datetime.now()) > data['updated'].replace(tzinfo=None):
-                count += 1
-                writer.writerow(data)
-    print("Zillow Contacts Success:",count)
-    return file, count
+    def sendEmail(self, receivers, file, count_fail, count_success):
+
+        sender = 'sos-api@team-sos.com'
+        receivers = receivers
+
+        port = 	587
+        user = 'sos-api@team-sos.com'
+        password = 'yuXn>jvuR7n*XQFH'
+
+        msg = MIMEMultipart()
+        msg['Subject'] = 'Tricon SMS Campaign Reporting'
+        msg['From'] = sender
+
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(self.file, "rb").read())
+
+        part.add_header('Content-Disposition', f"attachment; filename={self.file}")
+        msg.attach(part)
+
+        body = f"Good Morning, \n\n\tThe following data represents the number of records processed between {(datetime.datetime.now() - datetime.timedelta(days=self.numberDays)).strftime('%m-%d-%y %I:%M')} - {datetime.datetime.now().strftime('%m-%d-%y %I:%M')}. Please see the attached reports for additional record information:\n\n\n{self.campaign_name.title()}\n\n\tSuccess: {count_success}\n\n\tFailed: {count_fail}\n\n\tTotal: {count_fail+count_success}\n\nThank you!"
+        body = MIMEText(body) # convert the body to a MIME compatible string
+        msg.attach(body) # attach it to your main message
+
+        server = smtplib.SMTP('webmail.team-sos.com', timeout=6000)    
+
+        for i in receivers:
+            print(f"Sending to contact: {i}")
+            server.sendmail(msg['From'], to_addrs=i, msg=msg.as_string())
+
+def cleanUpReportFile(file):
+    import os
+    os.remove(file)
 
 
-def generate_3():
-    column_name = {'Number': '', 'First': '', 'Last': '', 'Address': '', 'City': '', 'State': '', 'Zip': '', 'PropertyID': '','SMSMessage1': '',  'updated': ''}
-    queryset = Campaign.objects.all()
-    
-    count = 0
-    data_set = Sent_Call_List.objects.all().filter(campaign=str('67544c3482904aa28cca1545ae9965d0')).values('Number', 'First', 'Last', 'Address', 'City', 'State', 'Zip', 'PropertyID','SMSMessage1',  'updated')
-    file = f"Rental_Forgiveness_Failed_{str(datetime.datetime.now()).replace(':', '_').replace('.', '_')}.csv"
-    with open(file, 'w+', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=column_name)
-        writer.writeheader()
-        
-        writer.writerow(column_name)
-        for data in data_set.order_by('updated'):
-            if (datetime.datetime.now() - datetime.timedelta(days=DAYS)) < data['updated'].replace(tzinfo=None) and (datetime.datetime.now()) > data['updated'].replace(tzinfo=None):
-                count += 1
-                writer.writerow(data)
-    print("Rental Forgiveness Failed:",count)
-    return file, count
+def processReport(campaign_name, columns, receivers, days):
+    report = GenerateReport(campaign_name, columns, days)
+    report.getCampaignId()
+    report.setReportFileName()
+    report.getSMSList()
+    file, successc, failc = report.writeSMSByFilteredDate()
+    report.sendEmail(receivers, file, successc, failc)
+    cleanUpReportFile(file)
 
 
-def generate_4():
-    column_name = {'Number': '', 'First': '', 'Last': '', 'Address': '', 'City': '', 'State': '', 'Zip': '', 'PropertyID': '','SMSMessage1': '',  'updated': ''}
-    queryset = Campaign.objects.all()
-    
-    count = 0
-    data_set = Sent_Call_List.objects.all().filter(campaign=str('2bc8a390424f4ce68222e4bab536264b')).values('Number', 'First', 'Last', 'Address', 'City', 'State', 'Zip', 'PropertyID','SMSMessage1',  'updated')
-    file = f"Zillow_Contacts_Failed_{str(datetime.datetime.now()).replace(':', '_').replace('.', '_')}.csv"
-    with open(file, 'w+', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=column_name)
-        writer.writeheader()
-        
-        writer.writerow(column_name)
-        for data in data_set.order_by('updated'):
-            if (datetime.datetime.now() - datetime.timedelta(days=DAYS)) < data['updated'].replace(tzinfo=None) and datetime.datetime.now() > data['updated'].replace(tzinfo=None):
-                count += 1
-                writer.writerow(data)
-                
-    print("Zillow Contacts Failed:",count)
-    return file, count
+def main():
+    receivers = ['tabbass@team-sos.com', 'tamerh.abbass@gmail.com', 'rthornton@team-sos.com', 'gknutson@triconresidential.com']
+
+    # Rental Forgiveness & Zillow Contacts Reports
+    columns = {'Number': '', 'First': '', 'Last': '', 'Address': '', 'City': '', 'State': '', 'Zip': '', 'PropertyID': '','SMSMessage1': '',  'updated': ''}
+    processReport('Rental Forgiveness', columns, receivers, days=7)
+    processReport('Zillow Contacts', columns, receivers, days=7)
+
+    # MF_Communications
+    columns = {'Number': '', 'First': '', 'PropertyID': '', 'SMSMessage1': '', 'updated': ''}
+    processReport('MF_Communications', columns, receivers, days=7)
 
 
-
-
-sender = 'sos-api@team-sos.com'
-<<<<<<< HEAD
-receivers = ['tabbass@team-sos.com', 'rthornton@team-sos.com', 'gknutson@triconresidential.com']
-=======
-receivers = ['tabbass@team-sos.com', 'tamerh.abbass@gmail.com']
->>>>>>> 7ee78a9d7ebb5b346c692dee810ff9194633f7b9
-
-port = 	587
-user = 'sos-api@team-sos.com'
-password = 'yuXn>jvuR7n*XQFH'
-
-
-def send_report():
-    msg = MIMEMultipart()
-    msg['Subject'] = 'Tricon SMS Campaign Reporting'
-    msg['From'] = sender
-    # msg['To'] = ','.join(receivers)
-
-    report1, rfsuccess = generate()
-    report2, zcsuccess = generate_2()
-    report3, rffail = generate_3()
-    report4, zcfail = generate_4()
-
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload(open(report1, "rb").read())
-    # Encoders.encode_base64(part)
-
-    part2 = MIMEBase('application', "octet-stream")
-    part2.set_payload(open(report2, "rb").read())
-
-    part3 = MIMEBase('application', "octet-stream")
-    part3.set_payload(open(report3, "rb").read())
-
-    part4 = MIMEBase('application', "octet-stream")
-    part4.set_payload(open(report4, "rb").read())
-
-    part.add_header('Content-Disposition', f"attachment; filename={report1}")
-    msg.attach(part)
-    part2.add_header('Content-Disposition', f"attachment; filename={report2}")
-    msg.attach(part2)
-    part3.add_header('Content-Disposition', f"attachment; filename={report3}")
-    msg.attach(part3)
-    part4.add_header('Content-Disposition', f"attachment; filename={report4}")
-    msg.attach(part4)
-
-    body = f"Good Morning, \n\n\tThe following data represents the number of records processed between {(datetime.datetime.now() - datetime.timedelta(days=DAYS)).strftime('%m-%d-%y %I:%M')} - {datetime.datetime.now().strftime('%m-%d-%y %I:%M')}. Please see the attached reports for additional record information:\n\nRental Forgiveness:\n\n\tTotal: {rfsuccess+rffail}\n\n\t Successful: {rfsuccess}\n\n\t Failed: {rffail}\n\nZillow Contacts:\n\n\tTotal: {zcsuccess+zcfail}\n\n\t Successful: {zcsuccess}\n\n\t Failed: {zcfail}"
-    body = MIMEText(body) # convert the body to a MIME compatible string
-    msg.attach(body) # attach it to your main message
-
-    server = smtplib.SMTP('webmail.team-sos.com', timeout=6000)    
-
-    for i in receivers:
-        print(f"Sending to contact: {i}")
-        server.sendmail(msg['From'], to_addrs=i, msg=msg.as_string())
+if __name__ == '__main__':
+    main()
